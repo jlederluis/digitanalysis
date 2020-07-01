e893faef7aaf2a821912179e0750c8472bab79ae
happy <wchang@caltech.edu>
2020-06-30 23:41:03 -0700

    made the main function and tested that it works. deleted some trash code

--- Rdigitanalysis/R/functions.R
+++ Rdigitanalysis/R/functions.R
@@ -4,14 +4,28 @@
 #Summer 2020
 ############################################################
 
+
 #clear workspace
 rm(list = ls())
 #force numerical representation rather than scientific
 options(scipen = 999)
 
+
 ############################################################
-#Data input function
+#Class function
 ############################################################
+
+
+#create our own class
+DigitAnalysis = setClass('DigitAnalysis', slots = c(raw="data.frame", cleaned="data.frame",
+                                                    numbers="data.frame", left_aligned="data.frame",
+                                                    right_aligned="data.frame"))
+
+
+############################################################
+#helper functions
+############################################################
+
 
 #this thing blow up my storage several times for some reason??????
 #library("readxl")
@@ -59,7 +73,7 @@ align_digits = function(indata, outdata, naming_method, align_direction='left', 
     outdata[[paste(colname, naming_method[i])]] = NA
     print(paste(colname, naming_method[i]))
   }
-  #print(head(outdata))
+
   for (j in 1:length(indata)){
     #split each number into chars
     chars = strsplit(as.character(indata[j]), "")[[1]]
@@ -70,103 +84,79 @@ align_digits = function(indata, outdata, naming_method, align_direction='left', 
     }
 
     for (k in 1:length(chars)){
-      #append them to table from the left/right
-      #print(chars)
-      # if (is.na(as.integer(chars[k]))){
-      #   print('oof')
-      # }
       outdata[[paste(colname, naming_method[k])]][j] = as.integer(chars[k])
-      #print(outdata[[paste(colname, naming_method[k])]][j])
     }
   }
   return(outdata)
 }
 
-#data format: row is observation; column is category
-col_analyzing = c("ALEXP","BENTOT", "BENM", "BENF")
 
-#raw input data->'raw' of the class
-raw_data = read.csv('C:\\Users\\happy\\OneDrive - California Institute of Technology\\Desktop\\ARID MASTER FINAL.csv')
+############################################################
+#Data input main function
+############################################################
 
-#clean up the number columns with numeric data to be analyzed->'cleaned' of the class
-cleaned_data = data.frame(raw_data) #make copy without pointer issue
-cleaned_data = drop_nan_empty(cleaned_data, col_analyzing)
+#parse and clean the data for digit analysis
+make_class = function(filepath, data_columns){
+  #data format: row is observation; column is category
+  #raw input data->'raw' of the class
+  raw_data = read.csv(filepath)
 
-#the dataframe with only the data being analyzed->'numeric' of the class
-numeric_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
+  #clean up the number columns with numeric data to be analyzed->'cleaned' of the class
+  cleaned_data = data.frame(raw_data) #make copy without pointer issue
+  cleaned_data = drop_nan_empty(cleaned_data, col_analyzing)
 
-#the dataframe with the left aligned digits of each data column to be analyzed->'left_aligned' of the class
-#i.e. a column is 'X'; first digit will be at column '1st digit' + 'X'
-left_aligned_column_names = c('1st digit', '2nd digit', '3rd digit', '4th digit', '5th digit', '6th digit', '7th digit',
-                              '8th digit', '9th digit', '10th digit', '11th digit', '12th digit', '13th digit')
-left_aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
+  #the dataframe with only the data being analyzed->'numeric' of the class
+  numeric_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
 
-#the dataframe with the right aligned digits of each data column to be analyzed->'right_aligned' of the class
-#i.e. a column is 'X'; first digit will be at column '1s' + 'X'
-right_aligned_column_names = c('1s', '10s', '100s', '1k', '10k', '100k', '1m', '10m', '100m', '1b', '10b', '100b', '1t')
-right_aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
+  #the dataframe with the left aligned digits of each data column to be analyzed->'left_aligned' of the class
+  #i.e. a column is 'X'; first digit will be at column '1st digit' + 'X'
+  left_aligned_column_names = c('1st digit', '2nd digit', '3rd digit', '4th digit', '5th digit', '6th digit', '7th digit',
+                                '8th digit', '9th digit', '10th digit', '11th digit', '12th digit', '13th digit')
+  left_aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
 
+  #the dataframe with the right aligned digits of each data column to be analyzed->'right_aligned' of the class
+  #i.e. a column is 'X'; first digit will be at column '1s' + 'X'
+  right_aligned_column_names = c('1s', '10s', '100s', '1k', '10k', '100k', '1m', '10m', '100m', '1b', '10b', '100b', '1t')
+  right_aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
 
-col_analyzing = c("ALEXP","BENTOT", "BENM", "BENF")
-##can be specified as both numeric_data='sdfsfsf' or numeric_data=c('dsdfsfsf') or or numeric_data=c('dsdfsfsf',...)
-for (i in 1:length(col_analyzing)) {
-  #name of current data column modifying
-  col_name = col_analyzing[i]
+  ##can be specified as both numeric_data='sdfsfsf' or numeric_data=c('dsdfsfsf') or or numeric_data=c('dsdfsfsf',...)
+  for (i in 1:length(col_analyzing)) {
+    #name of current data column modifying
+    col_name = col_analyzing[i]
 
-  #turn into numbers
-  cleaned_data[[col_name]] = as.numeric(gsub(",","",cleaned_data[[col_name]]))
+    #turn into numbers
+    cleaned_data[[col_name]] = as.numeric(gsub(",","",cleaned_data[[col_name]]))
 
-  #rounding
-  cleaned_data[[col_analyzing[i]]] = round_data(cleaned_data[[col_name]])
+    #rounding
+    cleaned_data[[col_analyzing[i]]] = round_data(cleaned_data[[col_name]])
 
-  #update 'numeric' object
-  numeric_data[[col_name]] = NA
-  numeric_data[[col_name]] = cleaned_data[[col_name]]
+    #update 'numeric' object
+    numeric_data[[col_name]] = NA
+    numeric_data[[col_name]] = cleaned_data[[col_name]]
 
-  #update 'align_left'
-  left_aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=left_aligned_data, naming_method=left_aligned_column_names, align_direction='left', colname=col_name)
+    #update 'align_left'
+    left_aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=left_aligned_data, naming_method=left_aligned_column_names, align_direction='left', colname=col_name)
 
-  #update 'align_right'
-  right_aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=right_aligned_data, naming_method=right_aligned_column_names, align_direction='right', colname=col_name)
+    #update 'align_right'
+    right_aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=right_aligned_data, naming_method=right_aligned_column_names, align_direction='right', colname=col_name)
+  }
 
-  #print(head(left_aligned_data))
+  #make our own class object finanlly!
+  DigitData = DigitAnalysis(raw = raw_data, cleaned = cleaned_data, numbers=numeric_data, left_aligned=left_aligned_data, right_aligned=right_aligned_data)
+
+  return(DigitData)
 }
 
-########
-#create our own class now, finally!
-DigitAnalysis = setClass('DigitAnalysis', slots = c(raw="data.frame", cleaned="data.frame",
-                                                    numbers="data.frame", left_aligned="data.frame",
-                                                    right_aligned="data.frame"))
+#############try it with given data
+col_analyzing = c("ALEXP","BENTOT", "BENM", "BENF")
+fp = 'C:\\Users\\happy\\OneDrive - California Institute of Technology\\Desktop\\ARID MASTER FINAL.csv'
 
-
-DigitData = DigitAnalysis(raw = raw_data, cleaned = cleaned_data, numbers=numeric_data, left_aligned=left_aligned_data, right_aligned=right_aligned_data)
-
-
-#some garbage
-
-# remove_by_index = function(array, index){
-#   return(array[as.integer(paste('-',as.character(index),sep = ""))])
-#
-# }
-# scientific_to_numeric = function(scientific_number){
-#   chars = tolower(strsplit(as.character(scientific_number), ""))
-#   print(chars)
-#   if (!is.na(match('e',chars))){
-#     index_e = match('e',chars)
-#     index_plus = match('+',chars)
-#     chars = remove_by_index(chars, index_e)
-#     chars = remove_by_index(chars, index_plus)
-#
-#     exponent = ''
-#     while (!is.na(chars[index_e])){
-#       exponent = paste(exponent,chars[index_e],"")
-#       chars = remove_by_index(chars, index_e)
-#     }
-#
-#     chars = append(chars, rep(0, as.integer(exponent)))
-#   }
-#   return(chars)
-# }
+DigitData = make_class(filepath = fp, data_columns = col_analyzing)
+head(DigitData@raw,2)
+head(DigitData@cleaned,2)
+head(DigitData@numbers,2)
+a=head(DigitData@left_aligned,2)
+b=head(DigitData@right_aligned,2)
 
 ######
 test = DigitAnalysis(cleaned = data.frame(matrix(ncol = 1, nrow = 1)), values=data.frame(matrix(ncol = 1, nrow = 1)))
@@ -178,8 +168,6 @@ test
 data.frame(1)
 slot(test, "cleaned")
 test@values
-
-
 
 x = 123.55
 trunc(x)
