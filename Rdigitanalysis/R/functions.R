2b68aa9999d9e3159a95c3a5338ef92fc112d853
happy <wchang@caltech.edu>
2020-07-01 00:11:06 -0700

    cleaned up code with cleaner functions

--- Rdigitanalysis/R/functions.R
+++ Rdigitanalysis/R/functions.R
@@ -23,7 +23,7 @@ DigitAnalysis = setClass('DigitAnalysis', slots = c(raw="data.frame", cleaned="d
 
 
 ############################################################
-#helper functions
+#basic helper functions
 ############################################################
 
 
@@ -71,7 +71,6 @@ align_digits = function(indata, outdata, naming_method, align_direction='left', 
   #intialize all digit places to NA
   for (i in 1:max){
     outdata[[paste(colname, naming_method[i])]] = NA
-    print(paste(colname, naming_method[i]))
   }
 
   for (j in 1:length(indata)){
@@ -92,34 +91,17 @@ align_digits = function(indata, outdata, naming_method, align_direction='left', 
 
 
 ############################################################
-#Data input main function
+#intermediate helper functions
 ############################################################
 
-#parse and clean the data for digit analysis
-make_class = function(filepath, data_columns){
-  #data format: row is observation; column is category
-  #raw input data->'raw' of the class
-  raw_data = read.csv(filepath)
 
-  #clean up the number columns with numeric data to be analyzed->'cleaned' of the class
+#clean up the number columns with numeric data to be analyzed->'cleaned' of the class
+make_cleaned_data = function(raw_data, col_analyzing){
   cleaned_data = data.frame(raw_data) #make copy without pointer issue
+
+  #drop rows with NaNs or empty strings in any numeric data column
   cleaned_data = drop_nan_empty(cleaned_data, col_analyzing)
 
-  #the dataframe with only the data being analyzed->'numeric' of the class
-  numeric_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
-
-  #the dataframe with the left aligned digits of each data column to be analyzed->'left_aligned' of the class
-  #i.e. a column is 'X'; first digit will be at column '1st digit' + 'X'
-  left_aligned_column_names = c('1st digit', '2nd digit', '3rd digit', '4th digit', '5th digit', '6th digit', '7th digit',
-                                '8th digit', '9th digit', '10th digit', '11th digit', '12th digit', '13th digit')
-  left_aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
-
-  #the dataframe with the right aligned digits of each data column to be analyzed->'right_aligned' of the class
-  #i.e. a column is 'X'; first digit will be at column '1s' + 'X'
-  right_aligned_column_names = c('1s', '10s', '100s', '1k', '10k', '100k', '1m', '10m', '100m', '1b', '10b', '100b', '1t')
-  right_aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
-
-  ##can be specified as both numeric_data='sdfsfsf' or numeric_data=c('dsdfsfsf') or or numeric_data=c('dsdfsfsf',...)
   for (i in 1:length(col_analyzing)) {
     #name of current data column modifying
     col_name = col_analyzing[i]
@@ -129,29 +111,90 @@ make_class = function(filepath, data_columns){
 
     #rounding
     cleaned_data[[col_analyzing[i]]] = round_data(cleaned_data[[col_name]])
+  }
+
+  return(cleaned_data)
+}
+
+#the dataframe with only the data being analyzed->'numeric' of the class
+make_numeric_data = function(cleaned_data, col_analyzing){
+
+  #initialization
+  numeric_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
+
+  for (i in 1:length(col_analyzing)) {
+    #name of current data column modifying
+    col_name = col_analyzing[i]
 
     #update 'numeric' object
     numeric_data[[col_name]] = NA
     numeric_data[[col_name]] = cleaned_data[[col_name]]
-
-    #update 'align_left'
-    left_aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=left_aligned_data, naming_method=left_aligned_column_names, align_direction='left', colname=col_name)
-
-    #update 'align_right'
-    right_aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=right_aligned_data, naming_method=right_aligned_column_names, align_direction='right', colname=col_name)
   }
 
-  #make our own class object finanlly!
+  return(numeric_data)
+}
+
+#the dataframe with the left/right aligned digits of each data column to be analyzed->'left_aligned'/'right_aligned' of the class
+make_aligned_data = function(cleaned_data, col_analyzing, naming_method, align_direction='left'){
+
+  #initialization
+  aligned_data = data.frame(matrix(ncol = 0, nrow = length(cleaned_data[,1])))
+
+  for (i in 1:length(col_analyzing)) {
+    #name of current data column modifying
+    col_name = col_analyzing[i]
+
+    #update by 'align_left' or 'align_right'
+    aligned_data = align_digits(indata=cleaned_data[[col_name]], outdata=aligned_data, naming_method=naming_method, align_direction=align_direction, colname=col_name)
+  }
+
+  return(aligned_data)
+}
+
+
+
+############################################################
+#Data input main function
+############################################################
+
+###parse and clean the data for digit analysis
+###col_analyzing can be specified as any of numeric_data='sdfsfsf' or numeric_data=c('dsdfsfsf') or numeric_data=c('dsdfsfsf',...)
+make_class = function(filepath, col_analyzing){
+  #data format: row is observation; column is category; must be csv!!!
+  #raw input data->'raw' of the class
+  raw_data = read.csv(filepath)
+
+  #hard-coded way of naming the digit places, should be sufficient, if not can further add
+  left_aligned_column_names = c('1st digit', '2nd digit', '3rd digit', '4th digit', '5th digit', '6th digit', '7th digit',
+                                '8th digit', '9th digit', '10th digit', '11th digit', '12th digit', '13th digit')
+  right_aligned_column_names = c('1s', '10s', '100s', '1k', '10k', '100k', '1m', '10m', '100m', '1b', '10b', '100b', '1t')
+
+  ########################creation of all sub-objects########################
+
+  cleaned_data = make_cleaned_data(raw_data, col_analyzing)
+
+  numeric_data = make_numeric_data(cleaned_data, col_analyzing)
+
+  #the dataframe with the left aligned digits of each data column to be analyzed->'left_aligned' of the class
+  #i.e. a column is 'X'; first digit will be at column '1st digit' + 'X'
+  left_aligned_data = make_aligned_data(cleaned_data, col_analyzing, naming_method=left_aligned_column_names, align_direction='left')
+
+  #the dataframe with the right aligned digits of each data column to be analyzed->'right_aligned' of the class
+  #i.e. a column is 'X'; first digit will be at column '1s' + 'X'
+  right_aligned_data = make_aligned_data(cleaned_data, col_analyzing, naming_method=right_aligned_column_names, align_direction='right')
+
+  ########################creation of DigitAnalysis class object########################
+
   DigitData = DigitAnalysis(raw = raw_data, cleaned = cleaned_data, numbers=numeric_data, left_aligned=left_aligned_data, right_aligned=right_aligned_data)
 
   return(DigitData)
 }
 
 #############try it with given data
-col_analyzing = c("ALEXP","BENTOT", "BENM", "BENF")
+data_columns = c("ALEXP","BENTOT", "BENM", "BENF")
 fp = 'C:\\Users\\happy\\OneDrive - California Institute of Technology\\Desktop\\ARID MASTER FINAL.csv'
 
-DigitData = make_class(filepath = fp, data_columns = col_analyzing)
+DigitData = make_class(filepath = fp, col_analyzing = data_columns)
 head(DigitData@raw,2)
 head(DigitData@cleaned,2)
 head(DigitData@numbers,2)
